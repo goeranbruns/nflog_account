f182b81abba5bfc63697d0e7d86ebf7ffe2a2458
Goeran Bruns <goeran@brunsware.de>
2025-01-09 11:31:42 +0100

    added copyright

--- nla_socket.h
+++ nla_socket.h
@@ -1,3 +1,23 @@
+/*
+    nflog_account (traffic accounting via netfilter_log)
+    nflog_account Copyright (C) 2024-2025 by Goeran Bruns
+*/
+
+/*
+    This file is part of nflog_account.
+    nflog_account is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    nflog_account is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with nflog_account. If not, see <https://www.gnu.org/licenses/>.
+*/
 #ifndef NLA_SOCKET_H
 #define NLA_SOCKET_H
 
